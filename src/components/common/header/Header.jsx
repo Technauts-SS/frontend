a8dc1ae1b35a3400fe96296f793cb32a6b1b0ddf
Yana <yanapvlnko@gmail.com>
2025-03-27 21:23:16 +0200

    new style for heder

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -40,7 +40,8 @@ function Header() {
         {/* Використовуємо Link з передачею state */}
         <Link to="/login" state={{ isLogin: true }} onClick={closeMenu}>Увійти</Link>
         <Link to="/register" state={{ isLogin: false }} onClick={closeMenu}>Зареєструватися</Link>
-        <Link to="/support" onClick={closeMenu}>Підтримати проект</Link>
+        {/* Видалено "Підтримати проект" */}
+        {/* <Link to="/support" onClick={closeMenu}>Підтримати проект</Link> */}
       </nav>
     </header>
   );
